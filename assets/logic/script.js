9e80b511683432c48d1d2b073a5a0572d8dbe53b
Enzo Trag <enzo.tragnone@gmail.com>
2022-05-23 16:13:25 -0400

    fixes and clean up to JS

--- assets/logic/script.js
+++ assets/logic/script.js
@@ -5,6 +5,7 @@ var numberArray = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
 var specialArray = ['"', "!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "]", "^", "_", "`", "{", "|", "}", "~"];
 var prePassword = [];
 var finalPassword = [];
+
 //mix arrays together
 var mix = function (confirm, mix) {
     if (confirm) {
@@ -13,27 +14,32 @@ var mix = function (confirm, mix) {
 }
 
 var passGenerate = function () {
+
     //define lenght of password and user input
     var passLenght = prompt("How long would you like the password to be? Pick a number between 1 and 128");
     if (!passLenght || passLenght < 8 || passLenght > 128) {
         window.alert("You must pick a number between 1 and 128!");
         return passGenerate();
     }
+
     //user input
     var upperConfirm = confirm("Would you like to add uppercase letters to your password?");
     var lowerConfirm = confirm("Would you like to add lowercase letters to your password?");
     var numberConfirm = confirm("Would you like to add numbers to your password?");
     var specialConfirm = confirm("Would you like to add special characters to your password?");
+
     //confirms user input
     if (!(upperConfirm || lowerConfirm || numberConfirm || specialConfirm)) {
         window.alert("You must pick one of the options.");
         return passGenerate();
     };
+
     //mesh of arrays
     mix(upperConfirm, upperCaseArray);
     mix(lowerConfirm, lowerCaseArray);
     mix(numberConfirm, numberArray);
     mix(specialConfirm, specialArray);
+
     //loop from random array
     while (finalPassword.length < passLenght) {
         random = prePassword[Math.floor(Math.random() * prePassword.length)];
@@ -41,6 +47,7 @@ var passGenerate = function () {
     };
 }
 
+//allows for multiple generate
 var reset = function () {
     finalPassword = []
 }
@@ -52,11 +59,14 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
+
     passGenerate();
+
     var password = finalPassword;
     var passwordText = document.querySelector("#password");
 
     passwordText.value = password;
+
     reset();
 
 }
